b1a503122819317656dba000ad5bf079f58a16c0
Alex Stegmann <DrRobotronic@users.noreply.github.com>
2016-03-29 09:08:58 -0400

    Added bomb scripts
    BombToss.js controls instantiation and velocity of bomb.
    BombScript.js controls bomb behaviour after instantiation.

---
+++ BombScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+	Destroy (gameObject, 2);
+}
+
+function FixedUpdate () {
+	//This can be used to fix the bomb to a Z position of 0 but it will push the dwarf back due to collision.
+	//transform.position.z = 0;
+}
+
+function OnTriggerEnter(other:Collider) {
+	if(other.tag == "enemy") {
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ BombToss.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public var bomb: Rigidbody;
+private var speed : float = 8.0;
+private var delay : int = 0;
+
+function Start () {
+
+}
+
+function Update () {
+	if(delay > 0) {
+		delay--;	
+	}
+	
+	if (Input.GetButtonDown("Fire1")) {
+		if(delay == 0) {	        
+	        var bombClone: Rigidbody = Instantiate(bomb, transform.position, transform.rotation);
+	        bombClone.velocity = transform.forward * speed;
+	        delay = 100;
+        }
+	}
+}
